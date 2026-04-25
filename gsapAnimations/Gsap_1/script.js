bd6bed1f26b5caa9dc3cd2c9dd5bf79259cb3df1
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-04-25 23:19:55 +0530

    Gsap animation 1

---
+++ gsapAnimations/Gsap_1/script.js
@@ -0,0 +1,27 @@
+gsap.to(".box1",{
+  x:1000,
+  duration:2,
+  delay:1,
+  rotate:360,
+})
+
+gsap.to('.box2',{
+  x:1000,
+  duration:2,
+  delay:3
+})
+
+gsap.to('.box3',{
+  x:1000,
+  duration:2,
+  delay:5,
+  backgroundColor:"green",
+  scale:0.5
+})
+
+
+
+
+
+// repeat : -1 from infinite animation 
+//stager:1 same type of multiple elements get the animation property only by one
