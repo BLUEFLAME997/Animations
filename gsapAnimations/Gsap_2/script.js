0c882c96001ea451a54374e7651cc664ddf77d0e
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-04-26 23:52:14 +0530

    Scroll trigger

---
+++ gsapAnimations/Gsap_2/script.js
@@ -0,0 +1,18 @@
+gsap.from('.page1 .box',{
+  scale:0.3,
+  duration:1,
+  opacity:0
+})
+
+gsap.from('.page2 .box',{
+  scale:0.3,
+  rotate:360,
+  duration:2,
+  opacity:0,
+  scrollTrigger:{
+    trigger:".page2 .box",
+    scroller:"body",
+    markers:true,
+    start:"top 60%"
+  }
+})
