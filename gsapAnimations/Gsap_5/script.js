26b7b7328c2d405a1194a1608b1c5a75da7d28d2
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-04-30 23:14:06 +0530

    Svg animation implementation

---
+++ gsapAnimations/Gsap_5/script.js
@@ -0,0 +1,8 @@
+let initialState = `M 10 100 Q 400 100 890 100`
+let finalState = `M 10 100 Q 400 100 890 100`
+
+let string = document.querySelector('.string')
+
+string.addEventListener('mousemove',(e)=>{
+  console.log(e)
+})
