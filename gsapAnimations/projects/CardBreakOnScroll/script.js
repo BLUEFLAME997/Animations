99b149557c732d6fce29607d0944906e10d5dd29
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-05-05 23:09:07 +0530

    Gsap card animation effect

--- gsapAnimations/projects/CardBreakOnScroll/script.js
+++ gsapAnimations/projects/CardBreakOnScroll/script.js
@@ -1,14 +1,25 @@
-gsap.from('.page1 h1',{
-  y:-40,
-  duration:1,
-  opacity:0
-})
-
-gsap.from('.page3 h1',{
-  y:-40,
-  duration:1,
-  opacity:0,
+let tl = gsap.timeline({
   scrollTrigger:{
-    trigger:".page3",
+    trigger:".sticky",
+    start:"top 0%",
+    end:"top -100%",
+    scrub:true,
+    marker:true,
+    pin:true
   }
-})
+});
+tl.to('.sticky .card-container',{
+  scale:0.87,
+}).to('.sticky .card',{
+  borderRadius:"20px"
+}).to('.sticky .card-container',{
+  gap:"20px"
+},"<").to('.sticky .card',{
+  rotateY:180
+}).to('.sticky #card-1',{
+  rotateZ:-15,
+  y:25
+},"<").to('.sticky #card-3',{
+  rotateZ:15,
+  y:25
+},"<")
