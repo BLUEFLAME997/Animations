e9f0b6d557b6450fe33bb898c20b6605314cf114
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-08-02 15:15:46 +0530

    Gsap learning

--- gsapAnimations/Gsap_7/script.js
+++ gsapAnimations/Gsap_7/script.js
@@ -10,12 +10,37 @@
 //   delay:0.6
 // })
 
-gsap.from('h1 span',{
-  yPercent:100,
-  duration:0.5,
-  opacity:0,
-  stagger:{
-    each:0.1,
-    from:"random"
-  }
-})
+// gsap.from('h1 span',{
+//   yPercent:100,
+//   duration:0.5,
+//   opacity:0,
+//   stagger:{
+//     each:0.1,
+//     from:"random"
+//   }
+// })
+
+const t1 = gsap.timeline();
+const val = 0.1
+
+
+
+t1.to(".box1",{
+  x:600,
+  duration:0.9,
+}).to(".box2",{
+  x:600,
+  duration:0.9,
+},"animation1").to(".box3",{
+  x:600,
+  duration:0.9,
+}).to(".box4",{
+  x:600,
+  duration:0.9,
+}).to(".box5",{
+  x:600,
+  duration:0.9,
+},`animation1-=${val}`)
+
+
+// <0.2 : adds 0.2s to the starting time of the previous one and becomes to the current one,-=0.3:ST2=ET1-0.3s,+=0.2:ST2=ET1+0.2
