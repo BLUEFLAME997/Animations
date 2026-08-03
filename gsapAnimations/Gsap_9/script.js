e3d9e9a8b301f4e6a3a2b9da6547e9b9f8372342
Shubham Giri <shubhamgiri.cs@gmail.com>
2026-08-03 18:27:18 +0530

    Scroll trigger animation

--- gsapAnimations/Gsap_9/script.js
+++ gsapAnimations/Gsap_9/script.js
@@ -1,11 +1,33 @@
-gsap.to('.box',{
-  x:700,
-  duration:1.6,
-  scrollTrigger:{
-    trigger:".page2",
-    start:"top top",
-    end:"top -100%",
-    scrub:true,
-    pin:true
-  }
+// gsap.to('.box',{
+//   x:700,
+//   duration:1.6,
+//   scrollTrigger:{
+//     trigger:".page2",
+//     start:"top top",
+//     end:"top -100%",
+//     scrub:true,
+//     pin:true
+//   }
+// })
+
+gsap.set('.imageContent', {
+  scale: 0.4
 })
+
+const tl = gsap.timeline({
+  // duration:2,
+  scrollTrigger: {
+    trigger: ".page2",
+    start: "top top",
+    end: "top -100%",
+    scrub: true,
+    pin: true
+  }
+});
+
+tl.to('.imageContent', {
+  scale: 1,
+  ease:"power2.inOut"
+}).to('.content', {
+  gap: "7rem"
+},"<")
